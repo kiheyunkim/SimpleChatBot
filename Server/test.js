cbf6f332d3a027141579b35a1e81e7a3d80e75a8
kiheyunkim <kiheyunkim@gmail.com>
2020-01-13 16:02:55 +0900

    add modules and modify client interface

--- Server/test.js
+++ Server/test.js
@@ -1,16 +1,6 @@
-var age = 28;
-var weight = 50.5;
-var gender='여';
-var isPretty= true;
-
-console.log(age);
-console.log(weight);
-console.log(gender);
-console.log(isPretty);
-
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
  
@@ -19,14 +9,22 @@ app.use(bodyParser.json())
 
 app.listen("8080",()=>{
     console.log("Start Start at 8080");
-})
-var desision = {'hi' : '안녕하세요'};
+});
+const desision = 
+{
+    'hi' : '안녕하세요',
+    '안녕': '나도 안녕',
+    '반갑습니다':'저도 반가워요',
+    '시리야': '저는 시리가 아니에요',
+    '뭐하니':'채팅해요',
+    '넌 뭘로 되있니?' : 'node.js'
+};
 
 app.post('/simplechatBot', (req, res)=>{
 
-    console.log(req.body);
-    var responseData = {'result' : desision[req.body.message]};
-    console.log(desision[req.body.message]);
-    res.json(responseData);
-    // 서버에서는 JSON.stringify 필요없음
+    //console.log(req.body);
+    var responseData = {'result' : desision[req.body.message] != undefined ? desision[req.body.message] : "올바르지 않은 요청입니다."};
+    //console.log(desision[req.body.message]);
+    res.json(responseData);     // 서버에서는 JSON.stringify 필요없음
+
 })
