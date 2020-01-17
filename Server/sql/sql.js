55a1e5b485744fcb34f0ac7bc717519dd3df0a0f
kiheyunkim <kiheyunkim@gmail.com>
2020-01-17 10:10:23 +0900

    add mysql keepalive interval

--- Server/sql/sql.js
+++ Server/sql/sql.js
@@ -8,11 +8,21 @@ const connectionValue={
 }
 
 let connection = mysql.createConnection(connectionValue);
+//50초 마다 의무적으로 쿼리를 보냄 = 끊김 방지
+let keepAliveInterval = setInterval(()=>{
+    connection.query('select 1',(error,results,fields)=>{
+        if(error)
+            throw error;
+
+        console.log('Keep alive query Sending');
+    })
+},50000);
 
 //db 연결이 끊어졌을 때 db 연결에 대해서 복구하기 위한 부분.
 connection.on('err',(err)=>{
     console.log('Database connection Error');
     if(err.code == 'PROTOCOL_CONNECTION_LOST'){
+        clearInterval(keepAliveInterval);
         connection = mysql.createConnection(connectionValue);
     }
 });
@@ -29,7 +39,7 @@ function GetResponse(request,sending){
                   'result' :""
             }
             
-            responseData['result'] = results.length==0 ? '올바르지 않은 값입니다' : results[0]['response'];
+            responseData['result'] = results.length==0 ? "등록되지 않은 질문입니다. 질문을 등록하려면 '$add,질문,답변' 형식으로 전송해주세요!" : results[0]['response'];
             sending.json(responseData);
         })
     })
