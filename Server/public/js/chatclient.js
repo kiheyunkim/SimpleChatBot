46b485ce73f83c957a98e89520f922de5cb6ecc0
kiheyunkim <kiheyunkim@gmail.com>
2020-01-30 09:50:46 +0900

    modify html to click Button & focus

--- Server/public/js/chatclient.js
+++ Server/public/js/chatclient.js
@@ -1,27 +1,27 @@
 let sendbutton;
 let messages;
 let max;
+let inputArea;
 
-window.onload = ()=>{
-    max = $("#box").height();
-    messages = document.getElementById('text')
-    sendbutton = document.querySelector('#messageSend');
+let AdjustCaretPoint =()=>{
+      let height = $("#text").height();
+      if(height > max) {
+          $("#box").scrollTop(height);
+      }
+  }
 
-    sendbutton.addEventListener('click', function(){
-        //버튼 id messageSend에 click 등록 및 그에 따른 반응 등록
-        let inputArea = document.querySelector('input');
-        let inputdata =inputArea.value;
-        AddMessage(`<div class = 'youMessage'>   ${inputArea.value} </div>`);
-        inputArea.value='';
-        sendMessage('/simplechatBot', inputdata);
-      });
+let AddMessage = (message)=>{
+  let newDiv = document.createElement('div');
+  newDiv.innerHTML = message;
+  messages.appendChild(newDiv);
+  AdjustCaretPoint();
 }
 
-function sendMessage(url, data){
+SendMessage = (msg, data) => {
   let jsonData = JSON.stringify({'message':data});  //Json 변환
   
   let xhr = new XMLHttpRequest();
-  xhr.open('POST', url);
+  xhr.open('POST', msg);
   xhr.setRequestHeader('Content-type', "application/json");  // content-type을 설정하고 데이터 송신
   xhr.send(jsonData); 
   
@@ -30,19 +30,28 @@ function sendMessage(url, data){
     console.log(xhr);
     AddMessage(`<div class = 'botMessage'>  ${result['result']} </div>`);
   });
-
 }
 
-function AddMessage(message){
-  let newDiv = document.createElement('div');
-  newDiv.innerHTML = message;
-  messages.appendChild(newDiv);
-  adjustCaretPoint();
-}
+$(document).ready(()=>{
+    max = $("#box").height();
+    messages = document.getElementById('text')
+    sendbutton = document.querySelector('#messageSend');
+    inputArea = document.querySelector('input');
+    $(inputArea).focus();
 
-function adjustCaretPoint(){
-      let height = $("#text").height();
-      if(height > max) {
-          $("#box").scrollTop(height);
-      }
-  }
+    sendbutton.addEventListener('click', function(){
+        //버튼 id messageSend에 click 등록 및 그에 따른 반응 등록
+        let inputdata =inputArea.value;
+        AddMessage(`<div class = 'youMessage'>   ${inputArea.value} </div>`);
+        inputArea.value='';
+        SendMessage('/simplechatBot', inputdata);
+      });
+
+      $(document).keyup((event)=>{
+        if(event.keyCode == '13'){
+          let inputArea = document.querySelector('input');
+          $(inputArea).focus();
+          $(sendbutton).click();
+        }
+      });
+});
