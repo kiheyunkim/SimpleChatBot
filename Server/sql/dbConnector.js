9159398d27e9d9a78985fdab91f3cfcd1545dc2e
Kihyeon Kim <kiheyunkim@gmail.com>
2021-08-05 22:17:51 +0900

    sql 질의 기능 로직 변경 및 파일 이름 변경

---
+++ Server/sql/dbConnector.js
@@ -0,0 +1,49 @@
+const mysql = require('mysql');
+
+const dbProperties = {
+	host: 'localhost',
+	user: 'root',
+	password: 'mysql',
+	database: 'chatbot'
+}
+
+let dbConnection = mysql.createConnection(dbProperties);
+
+
+let selectResponse = async (request) => {
+	return new Promise((resolve, reject) => {
+		dbConnection.query('SELECT * FROM MESSAGE WHERE request = ?', [request], (error, results, fields) => {
+			if (error) {
+				reject('error occurred');
+				return;
+			}
+
+			if (results.length === 0) {
+				resolve("등록되지 않은 질문입니다. 질문을 등록하려면 '$add,질문,답변' 형식으로 전송해주세요!");
+				return;
+			}
+
+			resolve(results[0]['response'])
+		});
+	});
+};
+
+let registerResponse = async (request, response) => {
+	return new Promise(((resolve, reject) => {
+		dbConnection.query('INSERT MESSAGE VALUE(?, ?)'), [request, response], (error, results, fields) => {
+			reject('error occurred');
+			return;
+
+			let affectRowsCount = results['affectedRows'];
+			if (affectRowsCount === 0) {
+				resolve('등록에 실패했습니다');
+				return;
+			}
+
+			resolve('등록되었습니다');
+		}
+	}));
+}
+
+exports.selectResponse = selectResponse
+exports.registerResponse = registerResponse
