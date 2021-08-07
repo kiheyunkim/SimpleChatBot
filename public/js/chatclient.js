ba743f688d1ae519f6c4476c552736fdc6ad874e
Kihyeon Kim <kiheyunkim@gmail.com>
2021-08-07 16:50:11 +0900

    챗봇 라벨 추가

--- public/js/chatclient.js
+++ public/js/chatclient.js
@@ -1,40 +1,44 @@
-let sendbutton;
-let messages;
-let max;
-let inputArea;
-
-let adjustCaretPoint = () => {
-	let height = $("#text").height();
-	if (height > max) {
-		$("#box").scrollTop(height);
-	}
-}
-
-let addMessage = (message, isMe) => {
-	let newDiv = document.createElement('div');
-	if (isMe) {
-		newDiv.setAttribute('id', 'you');
-	}
-	newDiv.innerHTML = message;
-	messages.appendChild(newDiv);
-	adjustCaretPoint();
-}
-
-let sendMessage = (msg, data) => {
-	let jsonData = JSON.stringify({'message': data});  //Json 변환
-
-	let xhr = new XMLHttpRequest();
-	xhr.open('POST', msg);
-	xhr.setRequestHeader('Content-type', "application/json");  // content-type을 설정하고 데이터 송신
-	xhr.send(jsonData);
-
-	xhr.addEventListener('load', function () {   // 데이터 수신에 대한 결과 출력
-		let result = JSON.parse(xhr.responseText);
-		addMessage(`<div class = 'botMessage'>  ${result.response} </div>`, false);
-	});
-}
-
 $(document).ready(() => {
+	let sendbutton;
+	let messages;
+	let max;
+	let inputArea;
+
+	let adjustCaretPoint = () => {
+		let height = $("#text").height();
+		if (height > max) {
+			$("#box").scrollTop(height);
+		}
+	}
+
+	let addMessage = (message, isMe) => {
+		let newDiv = document.createElement('div');
+		if (isMe) {
+			newDiv.setAttribute('id', 'you');
+		}
+		newDiv.innerHTML = message;
+		messages.appendChild(newDiv);
+		adjustCaretPoint();
+	}
+
+	let sendMessage = (msg, data) => {
+		let jsonData = JSON.stringify({'message': data});  //Json 변환
+
+		let xhr = new XMLHttpRequest();
+		xhr.open('POST', msg);
+		xhr.setRequestHeader('Content-type', "application/json");  // content-type을 설정하고 데이터 송신
+		xhr.send(jsonData);
+
+		xhr.addEventListener('load', function () {   // 데이터 수신에 대한 결과 출력
+			let result = JSON.parse(xhr.responseText);
+			addMessage(`
+				<p class="botLabel">bot</p>
+				<div class = 'botMessage'>  ${result.response} </div>`
+				, false);
+		});
+	}
+
+
 	max = $("#box").height();
 	messages = document.getElementById('text')
 	sendbutton = document.querySelector('#messageSend');
